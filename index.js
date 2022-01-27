33013ac85eaaa9876d8106ddb149375510360ae3
Alexandros Charangionis <alexandroscharangionis@gmail.com>
2022-01-27 17:58:53 +0200

    added playRound function & edited other log messages

--- index.js
+++ index.js
@@ -15,8 +15,8 @@ function computerPlay() {
 }
 
 // Assigning the function output to this variable.
-const computerSelection = computerPlay();
-console.log(computerSelection);
+let computerSelection = computerPlay();
+// console.log(computerSelection);
 
 // Prompting the user for an input and converting it to lowercase.
 let initialSelection = prompt("Select rock, paper or scissors: ");
@@ -27,13 +27,40 @@ let playerSelection;
 // Conditional statement that turns user input into a value from selection array.
 if (initialSelection == "rock") {
   playerSelection = selections[0];
-  console.log("You chose rock. Good luck.");
+  console.log(`You chose ${playerSelection.toLowerCase()}.`);
 } else if (initialSelection == "paper") {
   playerSelection = selections[1];
-  console.log("You chose paper. Good luck.");
+  console.log(`You chose ${playerSelection.toLowerCase()}.`);
 } else if (initialSelection == "scissors") {
   playerSelection = selections[2];
-  console.log("You chose scissors. Good luck.");
+  console.log(`You chose ${playerSelection.toLowerCase()}.`);
 } else {
-  console.log("Please submit valid input. Refresh and try again.");
+  console.log(
+    "You can only choose rock, paper or scissors. Refresh and try again."
+  );
 }
+
+// Function for an actual play round.
+function playRound(computerSelection, playerSelection) {
+  if (computerSelection === playerSelection) {
+    console.log(
+      `Computer chose ${playerSelection.toLowerCase()} too. Phew! It's a draw. Refresh for next round.`
+    );
+  } else if (
+    (computerSelection === selections[0] &&
+      playerSelection === selections[1]) ||
+    (computerSelection === selections[1] &&
+      playerSelection === selections[2]) ||
+    (computerSelection === selections[2] && playerSelection === selections[0])
+  ) {
+    console.log(
+      `Computer chose ${computerSelection.toLowerCase()}. You won! Refresh for next round.`
+    );
+  } else {
+    console.log(
+      `Computer chose ${computerSelection.toLowerCase()} and beat your ass. Nice try though. Refresh for next round.`
+    );
+  }
+}
+
+playRound(computerSelection, playerSelection);
